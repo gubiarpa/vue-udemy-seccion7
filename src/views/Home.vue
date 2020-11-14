0911e831c13929549a2cd1f9cab8f98d36d60597
gubiarpa <billy.arredondo@pucp.pe>
2020-11-13 19:49:22 -0500

    Uso de mapState

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h1>Mi contador {{ $store.state.contador }}</h1>
+    <h1>Mi contador {{ contador }}</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { mapState } from "vuex";
 
 export default {
   name: 'Home',
   components: {
+  },
+  computed: {
+    ...mapState(['contador'])
   }
 }
 </script>
